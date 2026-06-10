4519a8563d5022d26addf37f094f4b1920fa66d3
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-10 13:22:55 +0530

    Update CORS configuration to allow new origin

--- src/main/java/com/rekshinspringboot/firstSpring/config/CorsConfig.java
+++ src/main/java/com/rekshinspringboot/firstSpring/config/CorsConfig.java
@@ -38,7 +38,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://studentmanagement-springboot-1.onrender.com")
                         .allowedMethods("*");
             }
         };
